24d0410ff297cd163aea57c6ed84b2ca555d0bc0
sudeepb02 <sudeepbiswas02@gmail.com>
2017-07-01 00:52:58 +0000

    Add two large numbers using strings. Generalize to add 'n' large numbers

--- p013.cpp
+++ p013.cpp
@@ -21,6 +21,43 @@ string add(string &s1, string &s2)
 	string min, max, result;
 	result = "";
 
+	int i, sum, carry;
+	carry = 0;
+
+	if(s1.length() > s2.length())
+	{
+		max = s1;
+		min = s2;
+	}
+	else
+	{
+		max = s2;
+		min = s1;
+	}
+
+	for(i= min.length()-1; i>=0; i--)
+	{
+		sum = min[i] + max[i + max.length() - min.length()] + carry - 2*'0';
+
+		carry = sum/10;
+		sum %= 10;
+
+		result = (char)(sum + '0') + result;
+	}
+
+	i = max.length() - min.length() - 1;
+
+	while(i>=0)
+	{
+		sum = max[i] + carry - '0';
+
+		carry = sum/10;
+		sum %= 10;
+
+		result = (char)(sum + '0') + result;
+		i--;
+	}
+
 	return result;
 
 }
