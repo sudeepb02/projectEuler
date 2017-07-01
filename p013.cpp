a7575f2cb782b9d77612c180df89c4f5e75d9f2c
sudeepb02 <sudeepbiswas02@gmail.com>
2017-07-01 00:41:53 +0000

    Add large numbers using strings

---
+++ p013.cpp
@@ -0,0 +1,26 @@
+//Problem #13 : Large Sum
+#include <iostream>
+#include <string>
+using namespace std;
+
+string add(string&, string&);
+
+int main()
+{
+	string s1, s2, result;
+	cin>>s1>>s2;
+
+	result = add(s1, s2);
+
+	cout<<result;
+	return 0;
+}
+
+string add(string &s1, string &s2)
+{
+	string min, max, result;
+	result = "";
+
+	return result;
+
+}
