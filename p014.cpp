e72c3fcac96a91cbffe13ee07959b0bd4d6ce44a
sudeepb02 <sudeepbiswas02@gmail.com>
2017-07-02 00:57:02 +0000

    Solve #14 for smaller inputs. Segmentation fault for large inputs due to stack overflow. Replace recursion with iteration

--- p014.cpp
+++ p014.cpp
@@ -1,34 +1,61 @@
 //Problem #14 : Collatz Problem
-
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int numberOfSteps(int);
+long numberOfSteps(long);
+vector<long> steps(10000000, -1);
 
 int main()
 {
+	int t;
+	cin>>t;
 
+	for(int ti=0; ti<t; ti++)
+	{
+		long n;
+		cin>>n;
+		
+		//Base condition for recursive function
+		steps[0] = steps[1] = 0;
+
+		//Store number with maximum steps
+		long result = 1;
+		long maxsteps = 1;
+
+		for(int i=2; i<=n; i++)
+		{
+			if(numberOfSteps(i) >= maxsteps)
+			{
+				maxsteps = numberOfSteps(i);
+				result = i;
+			}
+		}
+
+		cout<<result<<endl;
+	}
+	return 0;
 }
 
-int numberOfSteps(int n)
+long numberOfSteps(long n)
 {
-	if(dictContains(n))
+	if(steps[n]!=-1)
 	{
-		return stepsOf(n);
+		return steps[n];
 	}
 	else
 	{
-		int steps = 0;
+		int s = 0;
 		if(n%2 == 0)
 		{
-			steps = 1 + numberOfSteps(n/2);
-			stepsOf.push(n, steps);
+			s = 1 + numberOfSteps(n/2);
+			steps[n] = s;
 		}
 		else
 		{
-			steps = 1 + numberOfSteps(3*n +1);
-			stepsOf.push(n, steps);
+			s = 1 + numberOfSteps(3*n +1);
+			steps[n] = s;
 		}
-		return steps;
+		return steps[n];
 	}
 }
