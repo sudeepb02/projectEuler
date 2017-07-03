a9bbe0b512d50e2de5fbc534cc4b8bc3482c83bc
sudeepb02 <sudeepbiswas02@gmail.com>
2017-07-03 02:10:59 +0000

    Add function for generating nCr

--- p015.cpp
+++ p015.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 using namespace std;
 
-//To generate nCr i.e C(n, r)
 long long Combination(int n, int r);
 
 int main()
@@ -12,18 +11,32 @@ int main()
 	//n rows, m columns
 	cin>>n>>m;
 
-	long long result;
-
-	result = Combination(m+n, m);
-
-	cout<<result;
+	cout<<Combination(m+n, m);
 
 	return 0;
 }
 
-
+//To generate nCr i.e C(n, r)
 long long Combination(int n, int r)
 {
-	long long answer;
-	return answer
+	vector< vector<long long>> cmat( n+1, vector<long long>(r+1, 0));
+
+	for(int i=0; i<=n; i++)
+	{
+		for(int j=0; j<=r && j<=i; j++)
+		{
+			//C(n, n) = 1 and C(n, 0) = 1
+			if(j==0 || j==i)
+			{
+				cmat[i][j] = 1;
+			}
+			else
+			{
+				//C(n, r) = C(n-1, r-1) + C(n-1, r)
+				cmat[i][j] = (cmat[i-1][j-1] + cmat[i-1][j]);
+			}
+		}
+	}
+
+	return cmat[n][r];
 }
