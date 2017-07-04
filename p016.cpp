ca5acbbdce6f1b54ee01e89969965c027a12834f
sudeepb02 <sudeepbiswas02@gmail.com>
2017-07-04 02:09:19 +0000

    Solve #16 : Power digit sum

---
+++ p016.cpp
@@ -0,0 +1,29 @@
+//Problem #16 : Power digit sum
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+using namespace std;
+
+int calculateSum(int);
+
+int main()
+{
+	int t;
+	cin>>t;
+
+	for(int ti=0; ti<t; ti++)
+	{
+		int n;
+		cin>>n;
+
+		cout<<calculateSum(n)<<endl;
+	}
+	return 0;
+}
+
+int calculateSum(int n)
+{
+	int result = -1;
+	
+	return result;
+}
