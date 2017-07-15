a7b6f3351cbbcdb3503b45aeabbe23119b5a6f3d
sudeepb02 <sudeepbiswas02@gmail.com>
2017-07-15 00:24:37 +0000

    Calculate the worth of each name

---
+++ p022.cpp
@@ -0,0 +1,44 @@
+//Problem #22 : Names Score
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	vector<string> names;	//To store all the names
+	vector<int> score(n);	//To store the score of names
+
+	string temp;
+
+	//Read and store the names
+	for(int i=0; i<n; i++)
+	{
+		cin>>temp;
+		names.push_back(temp);
+	}
+
+	//Sort the names alphabetically
+	sort(names.begin(), names.end());
+
+	//Calculate the score of each string
+	for(int i=0; i<n; i++)
+	{
+		int s = 0;
+		for(int j=0; j<names[i].length(); j++)
+		{
+			s = s + (names[i])[j] - 'A' + 1;
+		}
+		score[i] = s;
+	}
+	for(int i=0; i<n; i++)
+	{
+		cout<<names[i]<<"\t"<<score[i]<<endl;
+	}
+
+	return 0;
+}
